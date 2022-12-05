a638ffa6ad7a2e1012e2b5815b3bd8790e3ba5f0
Valmiki Vishwakarma <you@example.com>
2022-12-05 22:09:17 +0530

    adding changes

--- loops.js
+++ loops.js
@@ -2,3 +2,28 @@ for(var i=1;i<=10;i=i+2)
 {
     console.log("Hello World: "+i)
 }
+for(a=1;a<=5;a++){
+    console.log("Hey")
+}
+
+console.log('end of the line')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
