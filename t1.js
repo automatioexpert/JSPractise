91b6126d478d5c3e850e01a67df90798121c417f
Valmiki Vishwakarma <you@example.com>
2022-12-05 21:27:34 +0530

    Adding code changes

--- t1.js
+++ t1.js
@@ -3,3 +3,11 @@ console.log("Hello World")
 
 var i=200
 console.log(typeof i)
+
+var topCEO="Steve"
+console.log(typeof topCEO)
+
+//Conditional operator - && and  || OR operator ->returns true if any of the condition is true
+
+
+
