00e326ad3aaf5500f4444653d9543753516ad5e1
Valmiki Vishwakarma <you@example.com>
2022-12-05 22:43:35 +0530

    adding var code.

---
+++ varletandconst.js
@@ -0,0 +1,10 @@
+function  printout(){
+
+    for(var i=1;i<=10;i++){
+        console.log(i)
+    }
+}
+
+//console.log("Value if i outside the loop is:  "+i) //ReferenceError: i is not defined
+printout()
+console.log("Printing is done!!")
