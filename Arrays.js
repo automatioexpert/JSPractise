b0842638e299cfcc0894e9e313cf4ce4f51224a0
Valmiki Vishwakarma <you@example.com>
2022-12-05 22:37:15 +0530

    Pushing arrays.js

---
+++ Arrays.js
@@ -0,0 +1,13 @@
+//var first="Steve jobs story";
+//var first="sam story";
+
+//var ceo=["steve","sunder","mark","simon","satya nadella"]
+var ceo=new Array("steve","sunder","mark","simon","satya nadella")
+console.log(ceo[0])
+console.log(ceo.length)
+console.log(ceo[ceo.length-1])
+
+for(var i=0;i<ceo.length;i++){
+    console.log(ceo[i])
+}
+
