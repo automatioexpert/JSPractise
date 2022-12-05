29f3ce04062a2467083fb3895e5aecfe438fed4f
Valmiki Vishwakarma <you@example.com>
2022-12-05 22:17:41 +0530

    adding do while loop

--- whileandDoWhileLoop.js
+++ whileandDoWhileLoop.js
@@ -1,8 +1,19 @@
 var i=2
 
+/*
 while(i<=10){
     console.log(i)
     i++;
+} */
+
+//do while loop- do block will always be executed
+
+do{
+console.log("Executing do block")
+i++;
 }
 
-
+while(i<=10){
+    console.log('Executing while block')
+    //i++;
+}
