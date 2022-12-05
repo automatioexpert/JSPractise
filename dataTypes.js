1ca726de403433866dadcf923ea36170f8ba468f
Valmiki Vishwakarma <you@example.com>
2022-12-05 21:35:19 +0530

    Pushing dataTypes.js

---
+++ dataTypes.js
@@ -0,0 +1,13 @@
+/*
+There are 5 different data types in Java
+
+string,number,boolean,object,function
+
+There are 3 different data types in java 
+Object,date,Array
+
+Note: There are 2 data types that cannot contain values 
+
+null,undefined
+
+*/
