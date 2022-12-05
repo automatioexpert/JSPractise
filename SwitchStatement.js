87864a682cbd798b03894b0a3543582660ea90e7
Valmiki Vishwakarma <you@example.com>
2022-12-05 21:50:33 +0530

    Pushing switch case statement code.

---
+++ SwitchStatement.js
@@ -0,0 +1,20 @@
+var month="jan"
+
+switch(month){
+    case "dec":
+    console.log("Month selected as December");
+    break;
+
+    case "jan":
+        console.log("Month selected as jan")
+        break;
+   
+    case "feb":
+    console.log("Month selected as Fe")
+    break;
+
+    default:
+        console.log("Default month is March- Full of festival")
+
+
+}
