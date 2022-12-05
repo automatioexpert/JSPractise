7d1497f95d5d51b5f9327b84c9057d9d19c418ac
Valmiki Vishwakarma <you@example.com>
2022-12-05 21:42:46 +0530

    Pushing conditionalStatement.js

---
+++ conditionalStatement.js
@@ -0,0 +1,14 @@
+var a=400;
+var b=400;
+var c=a+b;
+
+
+//if else
+ if(a<b){
+console.log("B is greater: "+b)
+}else if(b>a) {
+    console.log("A is greater: "+a)
+}
+else{
+    console.log("Both are equal");
+}
