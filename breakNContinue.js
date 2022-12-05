c3e37a8aeaf733eb78a46d8ee5d942be81ad047b
Valmiki Vishwakarma <you@example.com>
2022-12-05 22:26:37 +0530

    Adding break and continue code.

---
+++ breakNContinue.js
@@ -0,0 +1,12 @@
+/*
+Break and continue--> Switch and continue statement-- will transfer the execution outside the loop
+
+Continue--> Loops-->Transfers the execution to the Next iteration
+
+*/
+for(var i=1;i<=10;i++){
+    if(i==5)
+    //break;
+    continue;
+    console.log(i);
+}
